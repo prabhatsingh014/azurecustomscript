bd97d409498ae156423e5069427e71d7e8128265
Prabhat Singh <54863845+prabhatsingh014@users.noreply.github.com>
2021-12-09 00:19:15 +0530

    Update customscriptextension2.sh

--- customscriptextension2.sh
+++ customscriptextension2.sh
@@ -1,5 +1,5 @@
-echo "Introduction" >> /var/www/html/Default.html
-echo "My Name is Prabhat Singh" >> /var/www/html/Default.html
-echo "I work as a Solution Architect" >> /var/www/html/Default.html
-echo "My hobbies are learning and travelling" >> /var/www/html/Default.html
-echo "This is an exercise of Custom Script Extension for Azure Linux VM" >> /var/www/html/Default.html
+echo "Introduction \n" >> /var/www/html/Default.html
+echo "My Name is Prabhat Singh. \n" >> /var/www/html/Default.html
+echo "I work as a Solution Architect. \n" >> /var/www/html/Default.html
+echo "My hobbies are learning and travelling. \n" >> /var/www/html/Default.html
+echo "This is an exercise of Custom Script Extension for Azure Linux VM. \n" >> /var/www/html/Default.html
