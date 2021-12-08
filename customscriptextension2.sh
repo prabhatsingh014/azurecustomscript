358d6e8554ddf62f6607868fd8c9e7c0bac18f90
Prabhat Singh <54863845+prabhatsingh014@users.noreply.github.com>
2021-12-09 00:16:11 +0530

    Add files via upload

---
+++ customscriptextension2.sh
@@ -0,0 +1,5 @@
+echo "Introduction" >> /var/www/html/Default.html
+echo "My Name is Prabhat Singh" >> /var/www/html/Default.html
+echo "I work as a Solution Architect" >> /var/www/html/Default.html
+echo "My hobbies are learning and travelling" >> /var/www/html/Default.html
+echo "This is an exercise of Custom Script Extension for Azure Linux VM" >> /var/www/html/Default.html
